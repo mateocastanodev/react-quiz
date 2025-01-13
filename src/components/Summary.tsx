ca06c954ab8dd6371bfce270e133d93ad5536061
Mateo Castano <m.castano1@uniandes.edu.co>
2025-01-13 09:40:04 -0500

    :construction: Adding percentage of the results

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,31 +4,49 @@ interface Summary {
   userAnswers: string[];
 }
 const Summary = ({ userAnswers }: Summary) => {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === questionsData[index].answers[0]
+  );
+
+  const skippedAnswersShare =
+    Math.round(skippedAnswers.length / userAnswers.length) * 100;
+  const correctAnswersShare =
+    Math.round(correctAnswers.length / userAnswers.length) * 100;
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
   return (
     <div id="summary">
       <img src={quizComplete} alt="Trophy icon" />
       <h2>Quiz completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswersShare}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswersShare}%</span>
           <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
       <ol>
         {userAnswers.map((answer: string, index: number) => {
+          let cssClass = "user-answer";
+          if (answer === null) {
+            cssClass += " skipped";
+          } else if (answer === questionsData[index].answers[0]) {
+            cssClass += " correct";
+          } else {
+            cssClass += " wrong";
+          }
           return (
             <li key={index}>
               <h3>{index + 1}</h3>
               <p className="question">{questionsData[index].text}</p>
-              <p className="user-answer">{answer}</p>
+              <p className={cssClass}>{answer ?? "Skipped"}</p>
             </li>
           );
         })}
