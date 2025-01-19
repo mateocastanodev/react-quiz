fdba8e9b423e040cdb068f8d5e31eca4324d5a02
Mateo <m.castano1@uniandes.edu.co>
2025-01-19 18:43:47 -0500

    ⚡️ Adding performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import MillionLint from "@million/lint";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    MillionLint.vite({
+      enabled: true,
+    }),
+    react(),
+  ],
   base: "/react-quiz/",
 });
